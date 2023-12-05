6c601a99276a6620bfe88c352ac7d54940c89c8b
AnzheloStoyanov <anjelostoqnov@gmail.com>
2023-12-05 09:41:57 +0200

    bahmu mamata

--- index.js
+++ index.js
@@ -9,7 +9,8 @@ const { error } = require('console')
 const app = express()
 const stripe = require("stripe")("sk_test_51N02adHGku6BOIoRkNHey0MqcixWhJiXNvQxpdPetJUTKMdpIFW9K8qqVkY3xNUT8mTfCuHopK7jtgkQ4ckYb5xt00rRGPJW5S");
 const bodyParser = require("body-parser")
-app.use(cors())
+app.use(cors({ origin: ['https://wood-and-roof-api-anzhelostoyanov.vercel.app', /\.regenci\.online$/], credentials: true }))
+
 app.use(express.json({ limit: '50mb' }))
 const server = require('http').createServer(app)
 const io = require('socket.io')(server, {
